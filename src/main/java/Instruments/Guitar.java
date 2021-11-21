fcf0ca30a552b7b626d760ca2c0c23e5a85b7dcb
jordanmckenna <jordanjmckenna@gmail.com>
2021-11-21 13:30:38 +0000

    Created instruments extending superclass and implementing interfaces.

--- src/main/java/Instruments/Guitar.java
+++ src/main/java/Instruments/Guitar.java
@@ -6,10 +6,11 @@ import Behaviours.ISell;
 public class Guitar extends Instrument implements IPlay, ISell {
     private int stringNumber;
 
-    public Guitar(String type, String colour, int wholesalePrice, int retailPrice) {
+    public Guitar(String type, String colour, int wholesalePrice, int retailPrice, int stringNumber) {
         super(type, colour, wholesalePrice, retailPrice);
         this.stringNumber = stringNumber;
     }
+
 
     public int getStringNumber() {
         return stringNumber;
--- src/main/java/Instruments/Sax.java
+++ src/main/java/Instruments/Sax.java
@@ -1,4 +1,28 @@
 package Instruments;
 
-public class Sax {
+import Behaviours.IPlay;
+import Behaviours.ISell;
+
+public class Sax extends Instrument implements IPlay, ISell {
+
+    private int reedNumber;
+
+    public Sax(String type, String colour, int wholesalePrice, int retailPrice, int reedNumber) {
+        super(type, colour, wholesalePrice, retailPrice);
+        this.reedNumber = reedNumber;
+    }
+
+    public int getReedNumber() {
+        return reedNumber;
+    }
+
+    @Override
+    public String play() {
+        return null;
+    }
+
+    @Override
+    public int calculateMarkup() {
+        return 0;
+    }
 }
