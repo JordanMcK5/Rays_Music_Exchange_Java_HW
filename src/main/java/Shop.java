d21f8d93042feb176de47e0c12db826b6e57909f
jordanmckenna <jordanjmckenna@gmail.com>
2021-11-21 13:43:40 +0000

    created shop class

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,21 @@
+import Behaviours.ISell;
+import Instruments.Instrument;
+
+import java.util.ArrayList;
+
 public class Shop {
+    private ArrayList<Instrument>stock;
+
+    public Shop(){
+        stock = new ArrayList<Instrument>();
+    }
+
+    public void addStock(Instrument instrument){
+        this.stock.add(instrument);
+    }
+
+    public void removeStock(Instrument instrument){
+        this.stock.remove(instrument);
+    }
+
 }
